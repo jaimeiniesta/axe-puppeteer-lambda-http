5655d27a9568112b78fb6d3cb7b1f0b94a075d7f
Jaime Iniesta <jaimeiniesta@gmail.com>
2019-02-22 14:25:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Axe-Puppeteer-Lambda-HTTP
+
+An AWS lambda function to use axe-core as an HTTP service using axe-puppeteer.
+
+## Deployment
+
+We use [Up](https://up.docs.apex.sh/) for deployment.
+
+* `npm install` will update the packages as specified in `package.json`.
+* `up start` will open a local development server.
+* `up` or `up deploy` will deploy to staging.
+* `up deploy production` will deploy to production.
+* `up url` will show the URL.
+* `up logs -f` to see the live logs.
---
+++ app.js
@@ -0,0 +1,65 @@
+const chromium = require('chrome-aws-lambda');
+const { AxePuppeteer } = require('axe-puppeteer');
+const puppeteer = require('puppeteer');
+
+const { createServer } = require('http');
+const { parse: parseURL } = require('url');
+
+const { PORT = 3000 } = process.env;
+
+const analyze = async url => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    headless: chromium.headless,
+    executablePath: await chromium.executablePath
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.setBypassCSP(true);
+
+    await page.goto(url);
+
+    const results = await new AxePuppeteer(page).analyze();
+
+    await page.close();
+    await browser.close();
+
+    return results;
+  } catch (err) {
+    console.error(err);
+
+    // Ensure we close the puppeteer connection when possible
+    if (browser) {
+      await browser.close();
+    }
+
+    // Re-throw
+    throw err;
+  }
+};
+
+const server = createServer((req, res) => {
+  // Ensure ?url= was provided
+  const { query = {} } = parseURL(req.url, true);
+  const { url } = query;
+  if (!url) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('URL required');
+    return;
+  }
+
+  // Analyze the URL
+  analyze(url)
+    .then(results => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(results, null, 2));
+    })
+    .catch(err => {
+      console.error('Runtime error', { error: err.message, stack: err.stack });
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(err.message || 'Unknown error');
+    });
+});
+
+server.listen(PORT);
